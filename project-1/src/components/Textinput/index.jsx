ab7e16f748162bf7ed8575e2e002d9ce35a80240
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-09-30 11:42:45 -0300

    Update index.jsx

--- project-1/src/components/Textinput/index.jsx
+++ project-1/src/components/Textinput/index.jsx
@@ -1,4 +1,4 @@
 import './style.css'
 export const TextInput = ({ searchValue, handleChange }) => {
-    return <input onChange={handleChange} value={searchValue} type="search" />;
+    return <input onChange={handleChange} value={searchValue} type="search" placeholder='Digite sua pesquisa'/>;
 };
